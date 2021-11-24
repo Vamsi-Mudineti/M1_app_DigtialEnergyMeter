62f1e9ba0e0e024b0fd8246da4b6e2ad21a1b3e1
Vamsi Mudineti <94225184+Vamsi-Mudineti@users.noreply.github.com>
2021-11-24 18:13:03 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Delay.h"
-#include "Delay.c"
+#include "/in/Delay.h"
+#include "/src/Delay.c"
 //#include<REG52.h>
 int delay();
 int Charging_step1();
